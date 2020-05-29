107e056469dd18e9de3e590669ab63ab47cb6eaf
Gorka Hernandez Estomba <gorka.hernandez@enverus.com>
2020-05-29 11:09:13 +0200

    work on contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,15 +1,58 @@
-import React from 'react';
+import * as React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import { CSSProperties } from "react"
+
+const styles: { [key: string]: CSSProperties } = {
+  form: {
+    width: 500,
+    margin: '0 auto',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  input: {
+    border: 'none',
+    outline: 'none',
+    backgroundColor: '#f1f1f1',
+    padding: '8px 16px',
+    borderRadius: 4,
+    fontFamily: 'Segoe UI'
+  },
+  textarea: {
+    border: 'none',
+    outline: 'none',
+    backgroundColor: '#f1f1f1',
+    padding: '8px 16px',
+    borderRadius: 4,
+    fontFamily: 'Segoe UI',
+    resize: 'vertical',
+    height: 150
+  },
+  formLabel: {
+    fontFamily: 'Segoe UI',
+    fontWeight: 500,
+    fontSize: '.7rem',
+    marginTop: 25
+  }
+}
 
 const Contact = () => (
   <Layout>
     <SEO title="Contact" />
-    <h1>Hi from the contact page</h1>
-    <p>Welcome to the contact page</p>
-    <Link to="/">Go back to the homepage</Link>
+    <h1 style={{ textAlign: 'center', fontSize: '4rem' }}>Hello</h1>
+    <p style={{ textAlign: 'center' }}>I'm <strong>Gorka</strong>, and I'd love to hear from you.</p>
+    <form action="" style={styles.form}>
+      <label style={styles.formLabel} htmlFor="contact-form-name">NAME</label>
+      <input style={styles.input} type="text" id="contact-form-name" placeholder={'Your name'}/>
+
+      <label style={styles.formLabel} htmlFor="contact-form-email">EMAIL</label>
+      <input style={styles.input} type="text" id="contact-form-email" placeholder={'Your email address'}/>
+
+      <label style={styles.formLabel} htmlFor="contact-form-message">MESSAGE</label>
+      <textarea style={styles.textarea} id="contact-form-message" placeholder={'What can I do for you?'}/>
+    </form>
   </Layout>
 );
 
